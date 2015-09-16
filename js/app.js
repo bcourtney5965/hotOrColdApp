b881a0022fe8248ab626376360fb7dc147747662
Brandon Courtney <brandon.h.courtney@gmail.com>
2015-09-15 22:57:43 -0500

    more work completed on submit functionality

--- js/app.js
+++ js/app.js
@@ -17,32 +17,55 @@ $(document).ready(function(){
   	var userGuess;
   	var feedbackClone = $("#feedback").clone();
 	var countClone = $("#count").clone();
+	// An array to store user's guesses
+	var guesses = [];
 
 	// Filter User Input Types
 	function checkInputType(userinput) {
 		var boolTest = userinput;
 		var convertedNumber = +userinput;
 		var checkNaN = isNaN(convertedNumber);
+		// This "catches" strings & booleans
 		if(checkNaN || typeof boolTest === "boolean") {
 			alert("That is not a number");
 		}
+		// This "catches" wrong sized numbers
 		if(convertedNumber >= 101 || convertedNumber <= 0) {
 			alert("Please enter a number between 1- 100");
 		}
+		// clears form-field after each submission
+		$("form")[0].reset();
 	}
+
+	// Pushes user's guess to array 
+	function pushToArray(guess) {
+		guesses.push(guess);
+	}
+
+	// append to guess list
+	// function listAppender(lastestGuess) {
+	// 	alert("47");
+	// 	$("ul #guessList").append("<li>" + lastestGuess + "</li>"):
+	// }
 
 	$("form").on("submit",function(event){
 		event.preventDefault();
+		// Obtains value from text-field
 		userGuess = $("#userGuess").val();
+		// Screens for data type
 		checkInputType(userGuess);
-		if (userinput(userGuess) === userGuess) {
-			alert("goodtimes!");
-		}
+		// Pushes guesses to array
+		pushToArray(userGuess);
+		alert("58");
+		// Appends guess to html page
+		// listAppender(userGuess);
+	});
+
+	$("a.new").click(function(){
+		alert(guesses);
 	});
 
 	/*
-	// An array to store user's guesses
-	var guesses = [];
 
 	// Function gets random number between 1 - 100
 	// And saved is to a variable
@@ -67,6 +90,7 @@ $(document).ready(function(){
 // Screen User Input Types
 // Append user guess
 // Push to guesses array
+// prevent larger numbers appending to "guesses" arrray
 
 
 /* --- Button Functionality Requirements --- */
