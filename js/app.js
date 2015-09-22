1ea34af788966b9715d7cf019b7b76ec34b0f173
Brandon Courtney <brandon.h.courtney@gmail.com>
2015-09-21 20:01:06 -0500

    added functionality that gives feedback relative to previous guess, and refactored

--- js/app.js
+++ js/app.js
@@ -13,9 +13,9 @@ $(document).ready(function(){
 
   	/*--- The start of my code ---*/
     //-------------------------------------------
-  	/*--- Declaring var's & cloning some initial info for later re-setting ---*/
+  	/*--- Declaring variables ---*/
   	var userGuess;
-  	// An array to store user's guesses
+  	// To store user's guesses
 	var guesses = [];
 	var secretNumber;
 
@@ -49,7 +49,9 @@ $(document).ready(function(){
 
 	// Function to get randomly generated secret number
 	function randomNumberGenerator() {
-		return Math.floor((Math.random() * 100) + 1);
+		var randomNumber = Math.floor((Math.random() * 100) + 1);
+		secretNumber = randomNumber;
+		return randomNumber;
 	}
 	secretNumber = randomNumberGenerator();
 
@@ -57,38 +59,56 @@ $(document).ready(function(){
 	function feedback(guess) {
 		var difference = Math.abs(secretNumber - guess);
 		var highOrLow = "";
-		// #feedback div / too low/too high
-		if (secretNumber !== guess) {
-			function tooLowOrHigh(guess) {
-				var output = "";
-				if (guess > secretNumber) {
-					output = "too high";
-				}
-				if (guess < secretNumber) {
-					output = "too low";
-				}
-				return output;
+		var closerFarther = "";
+
+		// #feedback div
+		// too low/too high
+		function tooLowOrHigh(guess) {
+			var output = "";
+			if (guess > secretNumber) {
+				output = "too high";
 			}
-			highOrLow = tooLowOrHigh(guess);
+			if (guess < secretNumber) {
+				output = "too low";
+			}
+			return output;
+		}
+
+		// #feedback div
+		// getting hotter/colder relative to last guess
+		function gettingHotterColder() {
+			var prevGuess = guesses.length - 2;
+			var currGuess = guesses.length - 1;
+			var prevGuessDist = Math.abs(secretNumber - guesses[prevGuess]);
+			var currGuessDist = Math.abs(secretNumber - guesses[currGuess]);
+			if (currGuessDist < prevGuessDist) {
+				closerFarther = " and getting hotter";
+			}
+			if (currGuessDist > prevGuessDist) {
+				closerFarther = " and getting colder";
+			}
+		}
+
+		if (guesses.length >= 2) {
+			gettingHotterColder();
 		}
 
 		if (secretNumber === guess) {
-			alert("new game?")
 		    $("#feedback").html("You guessed correctly! " + secretNumber + " is correct!");
 		} else if (difference >= 50) {
-		    $("#feedback").html("You are " + highOrLow + " and ice cold");
+		    $("#feedback").html("Your guess was " + tooLowOrHigh(guess) + "." + "  You are ice cold" + closerFarther + ".");
 		} else if (difference >= 30 && difference < 50) {
-		    $("#feedback").html("You are " + highOrLow + " and cold");
+		    $("#feedback").html("Your guess was " + tooLowOrHigh(guess) + "." + "  You are cold" + closerFarther + ".");
 		} else if (difference >= 20 && difference < 30) {
-		    $("#feedback").html("You are " + highOrLow + " and warm");
+		    $("#feedback").html("Your guess was " + tooLowOrHigh(guess) + "." + "  You are warm" + closerFarther + ".");
 		} else if (difference >= 10 && difference < 20) {
-		    $("#feedback").html("You are " + highOrLow + " and hot");
+		    $("#feedback").html("Your guess was " + tooLowOrHigh(guess) + "." + "  You are hot" + closerFarther + ".");
 		} else {
-		    $("#feedback").html("You are " + highOrLow + " and very hot");
+		    $("#feedback").html("Your guess was " + tooLowOrHigh(guess) + "." + "  You are very hot" + closerFarther + ".");
 		}
 	}
 
-	// append to guess list
+	// append most recent userGuess to ul list
 	function listAppender(lastestGuess) {
 		$("#guessList").append("<li>" + lastestGuess + "</li>");
 	}
@@ -98,6 +118,7 @@ $(document).ready(function(){
 		$("#feedback").html("Make your Guess!");
 		$("#guessList").empty();
 		$("#count").html(guesses.length);
+		randomNumberGenerator();
 	}
 
 	$("form").on("submit",function(event){
@@ -111,20 +132,10 @@ $(document).ready(function(){
 			userGuess = +prompt("Please try again");
 		}
 
-		// // Ensures proper input
-		// while (!(checkInputType(userGuess))) {
-		// 	userGuess = +prompt("Please provide a whole integer from 1 -100");
-		// }
-
-		// // Prevents someone making the same guess twice
-		// while (alreadyGuessed(userGuess)) {
-		// 	userGuess = +prompt("Number already guessed, try again");
-		// }
-
-		// Pushes guesses to array
+		// Push userGuess to array
 		pushToArray(userGuess);
 
-		// Appends guess to html page
+		// Appends userGuess to html page
 		listAppender(userGuess);
 
 		// #feedback provides feedback
@@ -136,111 +147,9 @@ $(document).ready(function(){
 		// clears form-field after each submission
 		$("form")[0].reset();
 	});
-
 	$("a.new").click(function() {
 		newGame();
 	});
 
-	
 
 });
-// enter a number
-
-// secret number
-
-// display hot/cold
-// display GETTING hotter/colder
-
-// push guess to array
-// append to guess webpage
-// alter number of guesses
-
-// clears form-field after each submission
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* --- Program's orger/logic --- */
-
-// prevent submitting ""
-// prevent guessing the same number twice
-// Utilize more rigorous logic for screening user input
-
-/* --- Button Functionality Requirements --- */
-// Guess Button
-//     - Hot/Cold feedback
-//     - Clear field
-// 	   - Append Guesses
-// $("#guessButton").click(function(event){
-// 			event.preventDefault();
-// + New Game Button
-//     reset:
-//     - Hot/Cold feedback
-//     - Clear field
-// 	   - Appended Guesses
-
-// Requirements
-
-
-
-
-
-/*
-
-	// Function gets random number between 1 - 100
-	// And saved is to a variable
-	var secretNumber = (function() {
-		return Math.floor((Math.random() * 100) + 1);
-	}());
-
-	// Append user guess to website
-
-	// Push to guesses array
-	guesses.push($("#userGuess").val());
-
-	// Determine if Hot/Cold
-
-	// Determine if getting Hott(er)/Cold(er)
-
-	*/
-
-
